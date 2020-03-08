6aa73a486a2c3a109c2f2d69c76751040456b18b
vplechuc <vplechuc@gmail.com>
2020-03-08 15:53:26 -0300

    creating flask app, define mine_block method

--- mod1-blockchain/blockchain.py
+++ mod1-blockchain/blockchain.py
@@ -62,3 +62,27 @@ class Blockchain:
             previous_block = current_block
             block_index += 1
         return True
+
+#definig the mining
+#create the flask app
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+
+    block = blockchain.create_block(proof, previous_hash)
+
+    response = {'message': 'Great block created',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof' : block['proof'],
+                'previous_hash': block['previous_hash']}
+
+    return jsonify(response) , 200
+
