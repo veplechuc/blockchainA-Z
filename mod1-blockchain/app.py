2aa56d93e7f83602e3833edaa38c38150240b086
vplechuc <vplechuc@gmail.com>
2020-03-12 17:10:14 -0300

    splitting block chain file for app and blockchain definition

---
+++ mod1-blockchain/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, jsonify
+from blockchain import Blockchain
+#definig the mining
+#create the flask app
+app = Flask(__name__)
+
+blockchain = Blockchain()
+
+@app.route('/mine_block', methods=['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+
+    block = blockchain.create_block(proof, previous_hash)
+
+    response = {'message': 'Great block created',
+                'index': block['index'],
+                'timestamp': block['timestamp'],
+                'proof' : block['proof'],
+                'previous_hash': block['previous_hash']}
+
+    return jsonify(response) , 200
+
+@app.route('/get_chain', methods=['GET'])
+def get_chain():
+    response = {'chain' : blockchain.chain,
+                'lenght': len(blockchain.chain)
+                }
+    return jsonify(response) , 200
+
+
+app.run(host='0.0.0.0', port = 5000)
