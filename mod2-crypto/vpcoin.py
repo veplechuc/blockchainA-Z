8973e5a289ca7717593aa203b695e4fdf4e58508
vplechuc <vplechuc@gmail.com>
2021-06-11 19:42:22 -0300

    adding transaccion and consensus logic

--- mod2-crypto/vpcoin.py
+++ mod2-crypto/vpcoin.py
@@ -18,16 +18,22 @@ class Blockchain:
     
     def __init__(self):
         self.chain = []
+        #first create a transaction before a block creation
+        self.transations = []
         #create gensis block
         self.create_block(proof=PROOF, previous_hash=INITIAL_HASH)
+        #handles all the nodes 
+        self.nodes = set()
 
     def create_block(self, proof, previous_hash):
         block = {'index':len(self.chain)+1,
                 'timestamp': str(datetime.datetime.now()),
                 'proof': proof,
                 'data': f'this fild just show some extra data, like index={len(self.chain)+1} and prev-hash={previous_hash}',
-                'previous_hash': previous_hash
+                'previous_hash': previous_hash,
+                'transactions': self.transations
                 }
+        self.transations = []        
         self.chain.append(block)
         return block
 
@@ -72,3 +78,36 @@ class Blockchain:
             previous_block = current_block
             block_index += 1
         return True
+
+    def add_transaction(self, sender, receiver, amount):
+        self.transations.append({
+            'sender': sender,
+            'receiver' : receiver,
+            'amount' : amount
+        })
+        #return the index of the block = previous block +1
+        previous_block = self.get_previous_block()
+        return previous_block['index'] + 1
+
+    def add_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
+#concesus resolver -- replace the chain with the longest one
+    def replace_chain(self):
+        net = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        #for eac node in the net we requests the  lenght of the chain
+        for node in net:
+            response = requests.get(f'http://{node}/get_chain')
+            if response.status_code == 200:
+                node_chain_length = response.json['length']
+                node_chain = response.json['chain']
+                if node_chain_length > max_length and self.is_chain_valid(node_chain):
+                    max_length = node_chain_length
+                    longest_chain = node_chain
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+
